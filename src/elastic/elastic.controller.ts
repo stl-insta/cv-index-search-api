fff0aff0d21bbf606a7efa5bbe9665385840f22e
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2021-10-18 14:53:42 +0200

    [FEAT] Implement delete index

--- src/elastic/elastic.controller.ts
+++ src/elastic/elastic.controller.ts
@@ -1,9 +1,34 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { elasticService } from './elastic.service';
+import url from 'url';
+
+export async function delete_index(req: Request, res: Response): Promise<void> {
+  const { name: indexName } = url.parse(req.url, true).query;
+
+  if (!indexName) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'No name given'
+    });
+  }
+
+  try {
+    const result = await elasticService.deleteIndex(<string>indexName);
+
+    res.status(StatusCodes.OK).json({
+      message: `Index ${indexName} deleted successfully`,
+      data: result
+    });
+  } catch (e) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: `Could not delete index ${indexName}`,
+      data: e
+    });
+  }
+}
 
 export async function create_index(req: Request, res: Response): Promise<void> {
-  const { index: indexName } = req.body;
+  const { name: indexName } = url.parse(req.url, true).query;
 
   if (!indexName) {
     res.status(StatusCodes.BAD_REQUEST).json({
@@ -12,14 +37,16 @@ export async function create_index(req: Request, res: Response): Promise<void> {
   }
 
   try {
-    await elasticService.createIndex(indexName);
+    const result = await elasticService.createIndex(<string>indexName);
 
     res.status(StatusCodes.OK).json({
-      message: `Index ${indexName} created successfully`
+      message: `Index ${indexName} created successfully`,
+      data: result
     });
   } catch (e) {
     res.status(StatusCodes.BAD_REQUEST).json({
-      message: `Could not create index ${indexName}`
+      message: `Could not create index ${indexName}`,
+      data: e
     });
   }
 }
