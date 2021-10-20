40a99b678828d0739c4b751597d962ce4d09eace
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2021-10-20 22:38:57 +0200

    [FEAT] Create filetype enums

---
+++ src/resources/constants/filetype.ts
@@ -0,0 +1,8 @@
+export enum FILETYPE {
+  // eslint-disable-next-line no-unused-vars
+  PDF = 'application/pdf',
+  // eslint-disable-next-line no-unused-vars
+  DOCX_1 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  // eslint-disable-next-line no-unused-vars
+  DOCX_2 = 'application/msword'
+}
